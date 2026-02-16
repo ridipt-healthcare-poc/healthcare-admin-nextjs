599e0b4705414e133363f27ab4344c8638f75fc4
Manoj Gayakwad <gaykwadmanoj1998@gmail.com>
2026-02-16 11:04:30 +0530

    added deployment changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_API_URL: "https://backend.medsparsh.com",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
